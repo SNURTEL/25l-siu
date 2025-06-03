541beac0ee5f111cab264bd07aded661dd4ebbdd
Wojciech Przybylski <01168910@pw.edu.pl>
2025-06-03 22:43:09 +0200

    Resources for multi v1

--- scripts/01-run_env.ps1
+++ scripts/01-run_env.ps1
@@ -5,8 +5,8 @@ docker run `
   -p 6080:80 `
   -e RESOLUTION=1920x1080 `
   --volume "${PWD}/code:/root/code" `
-  --volume "${PWD}/roads.png:/roads.png" `
-  --volume "${PWD}/routes.csv:/root/routes.csv" `
+  --volume "${PWD}/roads_multi.png:/roads.png" `
+  --volume "${PWD}/routes_multi.csv:/root/routes.csv" `
   --volume "${PWD}/models:/root/models" `
   --detach `
   dudekw/siu-20.04
