ab4c43dc065796f1cdced6b86d0de5b05752e69f
Daniel Tomala <danieltomala01@gmail.com>
2025-06-07 00:52:17 +0200

    Update play_multi

--- code/play_multi.py
+++ code/play_multi.py
@@ -18,11 +18,11 @@ class PlayMulti(DqnMulti):
         agents = self.env.reset(tnames=tnames, sections=['random' for tname in tnames]) # Losuje punkt startowy, aby zawsze zaczynać od pierwszego zmienić na 'default'
         current_states={tname:agent.map for tname,agent in agents.items()}
         last_states={tname:agent.map for tname,agent in agents.items()}
-        # env.tapi.setPen(tname,turtlesim.srv.SetPenRequest(off=0))   # Rysuj trasę
         
         while True:                 
             actions = {} 
             for tname in tnames:                                       
+                env.tapi.setPen(tname,turtlesim.srv.SetPenRequest(off=0))   # Rysuj trasę
                 control = np.argmax(self.decision(self.model, last_states[tname], current_states[tname])) # Decyzja ruchu
                 actions[tname] = self.ctl2act(control)
 
@@ -41,8 +41,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     env=turtlesim_env_multi.provide_env()                      # utworzenie środowiska
-    env.setup('routes.csv',agent_cnt=2)                   # połączenie z symulatorem
+    env.setup('routes.csv',agent_cnt=30)                       # połączenie z symulatorem
     env.SPEED_FINE_RATE = -5.0                                  # zmiana wybranych parametrów środowiska
+    env.DETECT_COLLISION = True
     agents=env.reset()                                          # ustawienie agenta
     tnames=list(agents.keys())                                  # 'lista agentów' do wytrenowania
     playMulti=PlayMulti(env, seed=None)                       # Reset seed na potrzeby testów i różnych miejsc początkowych          
